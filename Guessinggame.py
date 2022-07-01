a51d97c09975271a57090d6a1689b39297eabcd7
cardjr <limit7card@gmail.com>
2022-07-01 14:02:07 -0700

    guessinggame

--- Guessinggame.py
+++ Guessinggame.py
@@ -3,9 +3,36 @@ answer = 5
 print("Please guess number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("Please guess higher")
-elif guess > answer:
-    print("Please guess lower")
+if guess == answer:
+    print("You got it first time")
 else:
-    print("You got it first try")
+    if guess < answer:
+        print("Please guess higher")
+    else: # guess must be higher
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
+
+
+
+
+
+# if guess < answer:
+#     print("Please guess higher")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you've not guessed correctly")
+# elif guess > answer:
+#     print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you got the right answer")
+#     else:
+#         print("Sorry, you guessed incorrectly")
+# else:
+#     print("You got it first try")
