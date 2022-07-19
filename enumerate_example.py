70c8d7401abe958c234c60d8e1464d396b572f43
cardjr <limit7card@gmail.com>
2022-07-19 00:42:15 -0700

    guessinggame

---
+++ enumerate_example.py
@@ -0,0 +1,38 @@
+#for x, y in enumerate("abcdefgh"):
+#    print(x, y)
+
+
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac - Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flowers = []
+shrubs = []
+
+# write your code here
+for x in data:
+    if "Flower" in x:
+        flowers.append(x)
+    else:
+        shrubs.append(x)
+
+print(flowers)
+print(shrubs)
---
+++ lists_intro.py
@@ -0,0 +1,15 @@
+computer_parts = ["computer",
+                  "Monitor",
+                  "Keyboard",
+                  "mouse",
+                  "mouse mat"
+                  ]
+
+for b in computer_parts:
+    print(b)
+
+print()
+print(computer_parts[2])
+
+print(computer_parts[0:3])
+print(computer_parts[-1])
